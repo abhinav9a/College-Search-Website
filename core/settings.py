98dc37d08434446ef7c4d324519e96efbfb02f35
Abhinav <abhinav.raj.9a@gmail.com>
2023-01-16 23:57:35 +0530

    removed .env

--- core/settings.py
+++ core/settings.py
@@ -222,8 +222,8 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 EMAIL_HOST = 'smtp.sendgrid.net'
